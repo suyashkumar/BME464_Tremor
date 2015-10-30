00d0cefabce53b302a93fc08f2aa577a5e37ae51
Suyash Kumar <suyash.kumar@duke.edu>
2015-10-30 00:30:39 -0400

    Updated code to drive new servo motor from 0-40degrees for a 0-90 degree deflection.

---
+++ IMUTesting/IMUData_MotorControl2/src/IMUData.h
@@ -0,0 +1,17 @@
+/*
+ * IMUData.h
+ *
+ *  Created on: Sep 23, 2015
+ *      Author: suyashkumar
+ */
+
+#ifndef IMUDATA_H_
+#define IMUDATA_H_
+
+class IMUData {
+public:
+	IMUData();
+	virtual ~IMUData();
+};
+
+#endif /* IMUDATA_H_ */
