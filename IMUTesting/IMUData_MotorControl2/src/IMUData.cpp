e81b8adc86675d317033d92d2840ec0af5820914
Suyash Kumar <suyash.kumar@duke.edu>
2015-11-09 11:11:58 -0500

    Added csv logging capabilites

--- IMUTesting/IMUData_MotorControl2/src/IMUData.cpp
+++ IMUTesting/IMUData_MotorControl2/src/IMUData.cpp
@@ -29,77 +29,53 @@
 #include <unistd.h>
 #include <iostream>
 #include <exception>
+#include <fstream>
 
-mraa::Pwm* initPwm();
+// Forward function declarations:
+mraa::Pwm* initPwm(int pin);
+float calculateDutyCycle(float deflection);
+void motorControl(RTIMU_DATA imuData, mraa::Pwm* pwm);
+
 
 int main()
 {
-	// Parameter declaration
+	// Parameter declaration, initiation:
     int sampleCount = 0;
     int sampleRate = 0;
-    uint64_t rateTimer;
-    uint64_t displayTimer;
-    uint64_t now;
 
-    mraa::Pwm* pwm = initPwm(); // Initialize PWM Object
-	float duty_cycle=0.0;
+    mraa::Pwm* pwm = initPwm(0); // Initialize PWM Object
 
     RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
-
     RTIMU *imu = RTIMU::createIMU(settings);
-
     if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL)) {
         printf("No IMU found\n");
         exit(1);
     }
 
-    //  This is an opportunity to manually override any settings before the call IMUInit
-
+    // IMU Init:
     imu->IMUInit();
     imu->setSlerpPower(0.02);
     imu->setGyroEnable(true);
-    //imu->setAccelEnable(true);
-    //imu->setCompassEnable(true);
 
-
-    // Set up serial out UART ports
-    mraa_uart_context uart;
-    //uart = mraa_uart_init(0);
-    uart=mraa_uart_init_raw("/dev/ttyGS0");
-	mraa_uart_set_baudrate(uart, 9600);
-	if (uart == NULL) {
-		fprintf(stderr, "UART failed to setup\n");
-		return 0;
-	}
-	// char buffer[20]={}; // To hold UART output
+    // Main loop:
     while (1) {
-
         while (imu->IMURead()) {
             RTIMU_DATA imuData = imu->getIMUData();
-                printf("%s\r", RTMath::displayDegrees("Gyro", imuData.fusionPose));
-                float current_reading=imuData.fusionPose.x()*(180.0/3.14);
-                //sprintf(buffer,"%4f\n",imuData.fusionPose.x()*(180.0/3.14));
-                //mraa_uart_write(uart, buffer, sizeof(buffer)); // Write to UART
-                //duty_cycle=((current_reading/90)*0.05)+0.05;
-                duty_cycle=(current_reading/90); //scale from 0.5 to 0.7
-                duty_cycle=((duty_cycle*.2)+0.5);
-                pwm->config_percent(3,duty_cycle); // Write duty_cycle with period of 1ms
-                //pwm->period_us(200);
-                //pwm->pulsewidth_us(duty_cycle*200);
-                printf("\n\n");
-                fflush(stdout);
-
-
+            motorControl(imuData,pwm); // Update motor position
         }
-
     }
-    mraa_uart_stop(uart);
+    //mraa_uart_stop(uart);
   	mraa_deinit();
 }
-
-mraa::Pwm* initPwm(){
+/*
+ * initPwm()
+ * Initiates a PWM object on the supplied pin.
+ * @param pin an integer representing the pin to initiate PWM on.
+ * @returns pwm a mraa::Pwm* pointer
+ */
+mraa::Pwm* initPwm(int pin){
 	mraa::Pwm* pwm;
-	pwm=new mraa::Pwm(0);
+	pwm=new mraa::Pwm(pin);
 	if (pwm == NULL) { // Look for null case
 			//return MRAA_ERROR_UNSPECIFIED;
 			std::cerr << "PWM object was returned as null...exiting" << std::endl;
@@ -113,6 +89,45 @@ mraa::Pwm* initPwm(){
 	return pwm;
 
 }
+/*
+ * calculateDutyCycle()
+ * Calculates the duty cycle needed to move our servo to the provided deflection.
+ * @param deflection the input angular deflection
+ * @returns duty_cycle the duty cycle to send to the motor
+ */
+float calculateDutyCycle(float deflection){
+
+	return ((0.317*deflection)+52.464)/100;
+
+}
+
+/*
+ * motorControl()
+ * Inspects last read set of data from IMU and calculates a restoring displacement that is
+ * then translated to a duty cycle that is then sent to the motor. Any smoothing tasks occur
+ * in this function.
+ * @param imuData the RTIMU_DATA object that holds the last read set of data.
+ * @param pwm the PWM control object that writes the motor control signal.
+ */
+void motorControl(RTIMU_DATA imuData, mraa::Pwm* pwm){
+	std::ofstream logfile ("lastRun2.csv",std::ios::app);
+	printf("%s\r", RTMath::displayDegrees("Gyro", imuData.fusionPose));
+	float current_reading=imuData.fusionPose.y()*(180.0/3.14);
+	char buffer[20]={};
+	sprintf(buffer,"%f,",current_reading);
+
+	logfile << buffer;
+	            //mraa_uart_write(uart, buffer, sizeof(buffer)); // Write to UART
+	            //duty_cycle=((current_reading/90)*0.05)+0.05;
+	float duty_cycle=calculateDutyCycle(current_reading);
+	//printf("\nDuty cycle: %f\n",duty_cycle);
+	pwm->config_percent(3,duty_cycle); // Write duty_cycle with period of 1ms
+	printf("\n\n");
+	logfile.close();
+	fflush(stdout);
+}
+
+
 
 
 
